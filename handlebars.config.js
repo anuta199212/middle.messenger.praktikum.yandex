df11e414ad9d58e1bdc8dc09eba34318872414cf
Anna Shakhova <anuta199212@yandex.ru>
2022-08-24 16:57:52 +0700

    Added Chats list page
    Added dependencies
    
    -ESLint
    -Stylelint
    -Typescript
    
    Added Chats list page
    
    Modified 'handlebars.config.js'

--- handlebars.config.js
+++ handlebars.config.js
@@ -1,5 +1,12 @@
+let Handlebars = require('handlebars');
+
+Handlebars.registerPartial('meta', require('./src/partials/meta/meta.hbs'));
+Handlebars.registerPartial('button', require('./src/partials/button/button.hbs'));
+Handlebars.registerPartial('inputField', require('./src/partials/inputField/inputField.hbs'));
+Handlebars.registerPartial('inputFieldDisabled', require('./src/partials/inputField/inputFieldDisabled.hbs'));
+
 module.exports = {
-    partials: 'src/partials',
+    ...Handlebars,
     layouts: 'src/layouts',
     data: 'src/data',
-}
+};
